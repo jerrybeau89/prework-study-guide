1ee3bf76a535d90277bac7d54083635cedfc82b2
LionHeart <jerrybeau89@gmail.com>
2022-11-02 18:40:35 -0700

    3.4.4 JS adds.

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,30 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
+function listTopics() {
+  for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+  }
+}
+/*
 for(var x = 0; x < topics.length; x++) {
  console.log(topics[x]);
-}
+} */
 
-/*
-if (topic === 'HTML') {
+function selectTopic() {
+if (topics[0] === 'HTML') {
  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (topics[0] === 'CSS') {
  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (topics[0] === 'Git') {
  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+} else if (topics[0] === 'JavaScript') {
  console.log("Let's study JavaScript!");
 } else {
  console.log('Please try again!');
 }
-*/
+}
+
+listTopics();
+
+selectTopic();
 
